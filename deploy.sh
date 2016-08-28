d0ee57ffc691be4b6bcbe616298a35c14a524a52
William Osler <william@oslers.us>
2016-08-28 17:10:54 -0400

    create deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+# Make sure the current ruby version is installed
+export PATH="$HOME/.rbenv/bin:$PATH"
+rbenv install $(cat .ruby-version) --skip-existing
+
+bundle install --deployment
+rake build
+
+rsync -rlptODv --chmod o=rx --delete _site/ web.oslers.us:/var/www-blog/_site
+#ssh web.oslers.us chown -R william:www-data /var/www-blog
